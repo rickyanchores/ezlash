eef5441fae33d436dcbcdfa25f228e837b31ac8e
RickyAnchores <ricky23anchores@gmail.com>
2022-09-27 19:10:13 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>App</h1>
-      </header>
+      <h1>hello</h1>
     </div>
   );
 }
