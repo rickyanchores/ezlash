20b24534542e99c2f6a904cbb18c679a07a6c5d8
RickyAnchores <ricky23anchores@gmail.com>
2022-09-28 20:59:42 +0100

    edit

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div className='Home'>
         <h1>Home</h1>
-        <button className="btn">Click</button>
+        <img src="https://blog.artsper.com/wp-content/uploads/2014/04/robertmorrisLbeam.jpg" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta animi quis fugiat at minus dolore.</p>
+        <button className="btn">Click Here</button>
     </div>
   )
 }
